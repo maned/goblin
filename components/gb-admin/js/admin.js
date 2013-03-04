3d009d5bcf1e1737cad9edd483eed9af0acb7c0a
streetlight <streetlight@black-mamba.(none)>
2013-03-04 17:57:57 -0500

    Work on solving page storage issue

--- components/gb-admin/js/admin.js
+++ components/gb-admin/js/admin.js
@@ -1,3 +1,11 @@
+function utf8_to_b64( str ) {
+    return window.btoa(unescape(encodeURIComponent( str )));
+}
+ 
+function b64_to_utf8( str ) {
+    return decodeURIComponent(escape(window.atob( str )));
+}
+
 function setSubmitEvent() {
   $('#submit').click(function() {
       $.ajax({
@@ -15,7 +23,6 @@ function setSubmitEvent() {
           },
 
           success: function(data) {
-            console.log(data);
             console.log('process success');
          },
 
@@ -58,27 +65,39 @@ $(document).ready(function () {
 
   //Set up Change Event
   $('#page_to_edit').change(function() {
+      /*if ($(this).val() !== "new_page") {
+        
+      } else {
+        $('#admin-area').empty();
+            //Rebuild the template with the new data
+            var adminHTML = ich.page_admin_new();
+            //Append it
+            $('#admin-area').append(adminHTML);
+            //Set Submit Event
+            setSubmitEvent();
+      }*/
+      
       $.ajax({
-        url: "/page-edit.json",
-        type : "POST",
-        dataType: "json",
-        data: {
-            page_id: $(this).val()
-        },
-        success: function(data) {
-          //Empty the admin area
-          $('#admin-area').empty();
-          //Rebuild the template with the new data
-          var adminHTML = ich.page_admin(data);
-          //Append it
-          $('#admin-area').append(adminHTML);
-          //Set Submit Event
-          setSubmitEvent();
-       },
-        error: function() {
-          console.log('process error');
-        },
-      });
+          url: "/page-edit.json",
+          type : "POST",
+          dataType: "json",
+          data: {
+              page_id: $(this).val()
+          },
+          success: function(data) {
+            //Empty the admin area
+            $('#admin-area').empty();
+            //Rebuild the template with the new data
+            var adminHTML = ich.page_admin(data);
+            //Append it
+            $('#admin-area').append(adminHTML);
+            //Set Submit Event
+            setSubmitEvent();
+         },
+          error: function() {
+            console.log('process error');
+          },
+        });
   });
 
   //Initiate it By Default
