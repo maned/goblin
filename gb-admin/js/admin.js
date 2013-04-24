dc385043a30e9a57e2e3be999fbdf14f78606c59
streetlight <streetlight@black-mamba.(none)>
2013-04-23 21:32:37 -0400

    Work on reload fix

--- gb-admin/js/admin.js
+++ gb-admin/js/admin.js
@@ -17,23 +17,24 @@ function getAllPages() {
       type : "POST",
       dataType: "json",
       async: false,
-
       success: function(data) {
-          for (key in data) {
-            //Create a Closure because Javascript is strange, dude!
-            (function(key1) {
-              //In the looping, make sure you don't take the ID or Revision Number from the DB
-              if (key1 !== "_id" && key1 !== "_rev") {
-                //Add option to dropdown
-                $('#page_to_edit').append('<option value="' + key1 + '">' + b64_to_utf8(key1) + '</option>')
 
-              }
-             }
-            )(key)
-          }
+        console.log(data);
 
-        var page_selection_options = ich.page_options(data);
-        $('#page_to_edit').append(page_selection_options);
+        for (key in data) {
+          //Create a Closure because Javascript is strange, dude!
+          (function(key1) {
+            //In the looping, make sure you don't take the ID or Revision Number from the DB
+            if (key1 !== "_id" && key1 !== "_rev") {
+              //Add option to dropdown
+              $('#page_to_edit').append('<option class="option_values" value="' + key1 + '">' + b64_to_utf8(key1) + '</option>')
+            }
+           }
+          )(key)
+        }
+
+        $('#page_to_edit').append('<option value="new_page" id="new_page">Add a New Page...</option>');
+
      },
 
       error: function() {
@@ -42,15 +43,56 @@ function getAllPages() {
     });
 }
 
+function makeNewList(page_id_to_save) {
+
+  $.ajax({
+    url: "/page-edit.json",
+    type : "POST",
+    dataType: "json",
+    async: false,
+    data: {
+        page_id: page_id_to_save
+    },
+    success: function(data) {
+      //Empty the admin area
+      $('#admin-area').empty();
+      //Rebuild the template with the new data
+      var adminHTML = ich.page_admin(data);
+      //Append it
+      $('#admin-area').append(adminHTML);
+
+      //Set Submit Event
+      setSubmitEvent();
+
+      //Set up Delete Event
+      setDeleteEvent();
+
+      //Initate Admin Area
+      setAdminArea();
+
+      if ($('#page_id').val() !== "") {
+        $('#page_title').prop('disabled', true);
+        $('#page_url').prop('disabled', true);
+      }
+
+   },
+    error: function() {
+      console.log('Admin creation has failed. Please try again.');
+    },
+  });
+}
+
 function setSubmitEvent() {
   $('#submit').click(function() {
     //Get Beginning Id
     var begin_id = $('#page_id').val();
 
     //Automatically make page_id to be 100% sure for existing and for new ones!
-    $('#page_id').val(utf8_to_b64($('#page_title').val()));
+    var saved_id = utf8_to_b64($('#page_title').val());
+    console.log(saved_id);
+    $('#page_id').val(saved_id);
 
-    var page_id_to_save = $('#page_id').val();
+    var page_id_to_saveX = $('#page_id').val();
 
     //Check to Make sure it's not blank
     if ($('#page_id').val() !== "") {
@@ -60,7 +102,7 @@ function setSubmitEvent() {
           type : "POST",
           dataType: "json",
           data: {
-              page_id: page_id_to_save,
+              page_id: page_id_to_saveX,
               page_title: $('#page_title').val(),
               page_url: $('#page_url').val(),
               page_content: $('#page_content').val(),
@@ -72,16 +114,13 @@ function setSubmitEvent() {
 
             if (begin_id === "") {
 
-              //Reload the pages
-              //getAllPages();
-
-              //Correct the select field and launch change (TODO: Fix this.)
-             // $('#page_to_edit').change();
-
-              //Very bad fix.
-              window.location.reload();
-
               console.log('Page has been created.');
+
+              //getAllPages(saved_id);
+              //makeNewList(saved_id);
+
+              //TODO: Fix this.
+              window.location.reload();
 
             } else {
               console.log('Page has been updated.');
@@ -102,7 +141,8 @@ function setSubmitEvent() {
 
 function setDeleteEvent() {
   $('#delete').click(function() {
-    $.ajax({
+    if ($('#page_to_edit').val() !== "new_page" || $('#page_to_edit').val() === "SG9tZQ==") {
+      $.ajax({
           url: "/admin-delete.json",
           type : "POST",
           dataType: "json",
@@ -115,10 +155,11 @@ function setDeleteEvent() {
             console.log('Page has been deleted');
 
             //Re-get all pages.
-            getAllPages();
-            
+            //getAllPages();
+
             //Initiate change event to get a new admin.
-            $('#page_to_edit').change();
+            //$('#page_to_edit').val($('#page_to_edit option:first').val());
+            //makeNewList($('#page_to_edit').val());
 
             //Temp fix TODO: Fix this more cleanly.
             window.location.reload();
@@ -127,7 +168,11 @@ function setDeleteEvent() {
           error: function() {
             console.log('Page deletion has failed. Please try again.');
           },
-    });
+      });
+    } else {
+      alert('You cannot delete this.')
+    }
+    
   });
 }
 
@@ -145,42 +190,8 @@ $(document).ready(function () {
 
   //Set up Change Event
   $('#page_to_edit').change(function() {
-      
-      $.ajax({
-          url: "/page-edit.json",
-          type : "POST",
-          dataType: "json",
-          data: {
-              page_id: $(this).val()
-          },
-          success: function(data) {
-            //Empty the admin area
-            $('#admin-area').empty();
-            //Rebuild the template with the new data
-            var adminHTML = ich.page_admin(data);
-            //Append it
-            $('#admin-area').append(adminHTML);
-            //Set Submit Event
-            setSubmitEvent();
-
-            //Set up Delete Event
-            setDeleteEvent();
-
-            //Initate Admin Area
-            setAdminArea();
-
-            if ($('#page_to_edit').val() !== "new_page") {
-              $('#page_title').prop('disabled', true);
-              $('#page_url').prop('disabled', true);
-            }
-
-         },
-          error: function() {
-            console.log('Admin creation has failed. Please try again.');
-          },
-        });
+      makeNewList($(this).val());
   });
 
-  //Initiate it By Default
   $('#page_to_edit').change();
 });
