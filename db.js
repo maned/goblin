cd722518f9352b6696dba9e3c1416aaac9a9b3a9
streetlight <nweingartner@gmail.com>
2013-05-05 00:53:29 -0400

    Big update

--- db.js
+++ db.js
@@ -3,6 +3,6 @@ var cradle = require('cradle');
 var db = new(cradle.Connection)('http://127.0.0.1', 5984, {
 	auth: {username: 'admin', password: 'admin'},
 	cache: false
-}).database('gb_db_dynamic');
+}).database('dave');
 
 module.exports = db;
