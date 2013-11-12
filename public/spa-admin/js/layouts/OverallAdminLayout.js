433828b4c59964a535c9e136cc82783d4daaecbd
streetlight <email>
2013-11-12 07:20:00 -0500

    Update files

---
+++ public/spa-admin/js/layouts/OverallAdminLayout.js
@@ -0,0 +1,25 @@
+define([
+        'backbone',
+        'common',
+        'marionette'
+    ],
+    function (Backbone, Common) {
+
+        'use strict';
+
+        var GOB = Common.app_namespace || {};
+
+        GOB.Layouts.OverallAdminLayout = Backbone.Marionette.Layout.extend({
+
+            template: "#overall_admin_layout_template",
+
+            className: "main_container row",
+
+            regions: {
+                nav: "#admin-navigation",
+                page: "#admin-page"
+            }
+
+        });
+    }
+);
