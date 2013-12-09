c79428b6718dda588bf7064cce74b7647e781591
streetlight <email>
2013-12-09 13:25:12 -0500

    Client-side fix for page deletion

--- public/gb-admin/js/layouts/PageLayout.js
+++ public/gb-admin/js/layouts/PageLayout.js
@@ -119,17 +119,24 @@ define([
 
             deletePage: function () {
 
-                var that = this;
+                var that = this,
+                    pageToDelete = $('.page-to-edit-select').val();
 
-                this.model.deletePage(function () {
+                if (pageToDelete !== "SG9tZQ==") {
+                    this.model.deletePage(function () {
 
-                    alert('Page successfully deleted');
+                        alert('Page successfully deleted');
 
-                    that.render();
+                        that.render();
 
-                }, function (xhr) {
-                    console.log('Error deleting page. ' + xhr);
-                });
+                    }, function (xhr) {
+                        console.log('Error deleting page. ' + xhr);
+                    });
+                } else {
+                    alert('You cannot delete the index page.');
+                }
+
+
             }
 
         });
