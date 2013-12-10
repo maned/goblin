974b6d517ebd0acc36a29b2511b899d9098750c2
streetlight <nweingartner@gmail.com>
2013-12-10 14:31:56 -0500

    Remove chef folder

--- chef/solo.rb
+++
@@ -1,7 +0,0 @@
-log_level          :info
-log_location       STDOUT
-file_cache_path    File.expand_path(File.join(File.dirname(__FILE__), 'chef-tmp')) # '/var/chef/cookbooks'
-cookbook_path      [ File.expand_path(File.join(File.dirname(__FILE__), 'cookbooks')), 
-                     File.expand_path(File.join(File.dirname(__FILE__), 'site-cookbooks')) ]
-
-
